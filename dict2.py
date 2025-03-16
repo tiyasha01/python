8e1d3915f19063e7ace66b6af333fb62ba78a42d
tiyasha01 <tiyashadasadhikari2@gmail.com>
2025-03-16 07:48:25 -0700

    Add files via upload

---
+++ dict2.py
@@ -0,0 +1,12 @@
+marks={}
+
+x=int(input("enter phy:"))
+marks.update({"phy":x})
+
+y=int(input("enter maths:"))
+marks.update({"maths":y})
+
+z=int(input("enter chem:"))
+marks.update({"chem":z})
+
+print(marks)
